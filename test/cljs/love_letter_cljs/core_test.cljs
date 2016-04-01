f16f9818a7ecec91810a360708f580db61528185
Justin Watt <justinwatt@gmail.com>
2016-03-31 22:22:05 -0700

    Remove default test file

--- test/cljs/love_letter_cljs/core_test.cljs
+++
@@ -1,7 +0,0 @@
-(ns love-letter-cljs.core-test
-  (:require [cljs.test :refer-macros [deftest testing is]]
-            [love-letter-cljs.core :as core]))
-
-(deftest fake-test
-  (testing "fake description"
-    (is (= 1 2))))
--- test/cljs/love_letter_cljs/runner.cljs
+++ test/cljs/love_letter_cljs/runner.cljs
@@ -1,7 +1,5 @@
 (ns love-letter-cljs.runner
     (:require [doo.runner :refer-macros [doo-tests]]
-              [love-letter-cljs.core-test]
               [love-letter-cljs.game-test]))
 
-(doo-tests 'love-letter-cljs.core-test
-           'love-letter-cljs.game-test)
+(doo-tests 'love-letter-cljs.game-test)
